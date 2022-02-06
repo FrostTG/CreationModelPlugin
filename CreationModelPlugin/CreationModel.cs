84c4992c680ac68de49e04854c00cdc2cae5dd7e
Anton Gusev <FrostTG@gmail.com>
2022-02-06 19:56:01 +0300

    Добавлены методы создания дверей и окон

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -16,15 +17,61 @@ namespace CreationModelPlugin
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
-            Level level1 = GetLevel(doc,"Уровень 1");
-            Level level2 = GetLevel(doc,"Уровень 2");
+            Level level1 = GetLevel(doc, "Уровень 1");
+            Level level2 = GetLevel(doc, "Уровень 2");
             double width = 10000;
             double depth = 5000;
             List<XYZ> points = CreatePoints(width, depth);
-            List<Wall> walls =CreateWalls(doc, points, level1, level2);
-            
+            List<Wall> walls = CreateWalls(doc, points, level1, level2);
+
             return Result.Succeeded;
         }
+
+        private void AddWindow(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol windowType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("750 x 1200 мм"))
+                .Where(x => x.Family.Name.Equals("M_Подъемное-двустворчатое-окно"))
+                .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);           
+            XYZ point = (point2 + point1) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+            FamilyInstance familyWindow= doc.Create.NewFamilyInstance(point, windowType, wall, level1, StructuralType.NonStructural);
+            double headHeight = familyWindow.get_Parameter(BuiltInParameter.INSTANCE_HEAD_HEIGHT_PARAM).AsDouble();
+            double sillHeight = familyWindow.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM).AsDouble();
+            double wallHeight = wall.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble();
+            familyWindow.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM).Set(wallHeight / 2 - (headHeight - sillHeight) / 2);
+
+        }
+
+        private void AddDoor(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol doorType = new FilteredElementCollector(doc)
+                 .OfClass(typeof(FamilySymbol))
+                 .OfCategory(BuiltInCategory.OST_Doors)
+                 .OfType<FamilySymbol>()
+                 .Where(x => x.Name.Equals("0915 x 2134 мм"))
+                 .Where(x => x.Family.Name.Equals("M_Однопольные-Щитовые"))
+                 .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!doorType.IsActive)
+                doorType.Activate();
+            doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
+        }
+
         public Level GetLevel(Document doc, string levelName)
         {
             List<Level> listLevel = new FilteredElementCollector(doc)
@@ -37,10 +84,10 @@ namespace CreationModelPlugin
                 .FirstOrDefault();
             return level;
         }
-        public List<XYZ> CreatePoints (double width, double depth)
+        public List<XYZ> CreatePoints(double width, double depth)
         {
-            double width1= UnitUtils.ConvertToInternalUnits(width, UnitTypeId.Millimeters);
-            double depth1= UnitUtils.ConvertToInternalUnits(depth, UnitTypeId.Millimeters);
+            double width1 = UnitUtils.ConvertToInternalUnits(width, UnitTypeId.Millimeters);
+            double depth1 = UnitUtils.ConvertToInternalUnits(depth, UnitTypeId.Millimeters);
             double dx = width1 / 2;
             double dy = depth1 / 2;
             List<XYZ> points = new List<XYZ>();
@@ -50,8 +97,8 @@ namespace CreationModelPlugin
             points.Add(new XYZ(-dx, dy, 0));
             points.Add(new XYZ(-dx, -dy, 0));
             return points;
-        }  
-        public List<Wall> CreateWalls(Document doc, List<XYZ> points, Level level1, Level level2 )
+        }
+        public List<Wall> CreateWalls(Document doc, List<XYZ> points, Level level1, Level level2)
         {
             List<Wall> walls = new List<Wall>();
 
@@ -66,6 +113,10 @@ namespace CreationModelPlugin
                     walls.Add(wall);
                     wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
                 }
+                AddDoor(doc, level1, walls[0]);
+                AddWindow(doc, level1, walls[1]);
+                AddWindow(doc, level1, walls[2]);
+                AddWindow(doc, level1, walls[3]);
                 ts.Commit();
             }
             return walls;
